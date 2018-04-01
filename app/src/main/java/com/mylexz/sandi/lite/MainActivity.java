7484aa2a524afb49e3121af4196ffd70dac74616
AlexzPurewoko <purwoko908@gmail.com>
2018-04-01 13:09:46 +0700

    Initial commit

---
+++ app/src/main/java/com/mylexz/sandi/lite/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mylexz.sandi.lite;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
